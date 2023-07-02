dd305628d4815ac4097b6a0a0c6f5ff514c40f91
femibiki273@gmail.com <femibiki273@gmail.com>
2023-07-02 11:58:10 +0300

    newchange

--- hackaton/src/App.js
+++ hackaton/src/App.js
@@ -5,6 +5,7 @@ import About from './components/About'
 import Course from './components/Course'
 import Contact from './components/Contact'
 import All from './components/All'
+import Signin from './components/Signin';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/about" element={<About />}/>
         <Route path="/course"  element={<Course/>}/>
         <Route path="/contact" element={<Contact/>}/>
+        <Route path='/signin' element={<Signin/>}/>
       </Routes> 
     </Router>
   );
---
+++ hackaton/src/components/Signin.js
@@ -0,0 +1,84 @@
+import React, { useState} from 'react'
+import './Signin.css'
+import { Link } from 'react-router-dom'
+
+
+function Signin() {
+    const[toggle,setToggle]=useState(false);
+    const togglePasswordVisibility = () => {
+        setToggle(!toggle);
+      };
+      const [role, setRole] = useState("");
+
+      const handleRoleChange = (event) => {
+        setRole(event.target.value);
+      };
+    
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        // Handle login logic based on the selected role
+        if (role === "student") {
+          // Logic for student login
+          console.log("Student login");
+        } else if (role === "admin") {
+          // Logic for admin login
+          console.log("Admin login");
+        } else {
+          // No role selected
+          console.log("Please select a role");
+        }
+      };
+  return (
+    <div className='parent'>
+      <div className='parent-back'>
+            <div className='back-log'>
+                <h1>&lt;FE<span>N</span>OTE&gt;</h1>
+            </div>
+      </div>
+      <div className='parent-log'>
+        <div className='def' onSubmit={handleSubmit}>
+            <div className='def1'> 
+                <label>
+                    Student
+                    <br />
+                    <input type="radio" value="student"checked={role === "student"}
+                    onChange={handleRoleChange}
+                    />
+                </label>
+            </div>
+            <div className='def2'>
+                <label>
+                    Admin
+                    <br />
+                    <input type="radio" value="admin" checked={role === "admin"} onChange={handleRoleChange}
+                    />
+                </label>
+            </div>
+        </div>
+        <h2>Welcome Back!</h2>
+            <div className='login'>
+                <form>
+                    <label>
+                        Email<br/>
+                        <input type="email" placeholder="example@gmail.com" />
+                    </label>
+                    <label>
+                        Password<br/>
+                        <input type={toggle ? "text" : "password"} placeholder='********'/>
+                        <i class={toggle? "far fa-eye-slash" : "far fa-eye"} onClick={togglePasswordVisibility}></i><br/>
+                        <span ></span>
+                        {/* <small style={{color:"red"}}>*This field is required.</small><br/> */}
+                        <p><Link to="/">Forgot password?</Link></p>
+                    </label>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        <div className='next'>
+            <p>Not a member?<Link to="/signup">Sign up now</Link></p>
+        </div>
+        </div>
+      </div>
+  )
+}
+
+export default Signin
